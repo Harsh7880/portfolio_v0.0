4c6032c77669e5995ecd0e95c5ad1797ddf3e0be
Harsh Kumar <vijayk8478@gmail.com>
2024-09-19 12:00:05 +0530

    Hero page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import About2 from "./components/About2";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
@@ -6,20 +6,27 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-900 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
-        <About2 />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/work" element={<Technologies />} />
+          </Routes>
+        </BrowserRouter>
         <Technologies />
         <Experience />
         <Projects />
-        <Contact />
+        <Contact /> */
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import { HERO_CONTENT } from "../utils/constants";
-// import profilePic from "../assets/kevinRushProfile.png";
 import { motion } from "framer-motion";
-import harsh from "../assets/harsh.webp";
+import harsh from "../assets/harsh5.png";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
+import instagram from "../assets/instagram.png";
+import twitter from "../assets/x.png";
+import email from "../assets/gmail.png";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -17,42 +21,77 @@ const Hero = () => {
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
+          <div className="flex flex-col items-center lg:items-start ml-20">
             <motion.h1
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-bold tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-2 text-6xl font-bold text-white/80 tracking-tight lg:mt-16 lg:text-7xl"
             >
-              Harsh Kumar
+              Hello I am Harsh Kumar
             </motion.h1>
-            <motion.span
+
+            <motion.p
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
-            >
-              Full Stack Developer
-            </motion.span>
-            <motion.p
-              variants={container(1)}
-              initial="hidden"
-              animate="visible"
-              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+              className="my-2 max-w-xl py-4 font-medium text-2xl"
             >
               {HERO_CONTENT}
             </motion.p>
+            <motion.div
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="flex items-center justify-center gap-4 text-2xl"
+            >
+              <a
+                href="https://www.linkedin.com/in/harshkumar7880/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="w-7" src={linkedin} alt="linkdin" />
+              </a>
+              <a
+                href="https://github.com/Harsh7880"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="w-8" src={github} alt="github" />
+              </a>
+              <a
+                href="https://www.instagram.com/harsh_7880/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="w-8" src={instagram} alt="instagram" />
+              </a>
+              <a
+                href="https://x.com/harshk8478"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="w-8" src={twitter} alt="twitter" />
+              </a>
+              <a
+                href="vijayk8478@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="w-8" src={email} alt="email" />
+              </a>
+            </motion.div>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex justify-center">
+          <div className="flex">
             <motion.img
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.2 }}
+              transition={{ duration: 1, delay: 0.5 }}
               src={harsh}
               alt="Harsh Kumar"
-              className="rounded-[10%] h-[500px]"
+              className="-mt-20 w-[80%]"
             />
           </div>
         </div>
